37ae5c98a2a47c87417710e1387db7e47e360779
Izlal Chaidar Islam <117796672+lalz7@users.noreply.github.com>
2024-10-09 10:06:44 +0700

    Update Rule.java

--- DetectDiseasesCornPlants-main/src/Rule.java
+++ DetectDiseasesCornPlants-main/src/Rule.java
@@ -1,15 +1,7 @@
 
 import java.util.List;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author AERO
- */
 public class Rule {
     private List<String> antecedent;
         private String consequent;
